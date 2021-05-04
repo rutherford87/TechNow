96d2b2aceed58aaa659aa48da616a9c41aab7689
Tyler Rutherford <rutherford87@gmail.com>
2021-05-03 21:53:27 -0500

    added model, seeds, and routes for comments table

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
     });
 
     // Serialize data so the template can read it
-    const posts = Data.map((post) => post.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
@@ -36,12 +36,18 @@ router.get('/post/:id', async (req, res) => {
           attributes: ['name'],
         },
       ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['comment', 'name'],
+        }
+      ]
     });
 
     const post = postData.get({ plain: true });
-
+console.log(postData);
     res.render('post', {
-      ...post,
+      ...postData,
       logged_in: req.session.logged_in
     });
   } catch (err) {
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -16,6 +17,11 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
+
+  const comments = await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+    returning: true,
+  });  
 
   process.exit(0);
 };
